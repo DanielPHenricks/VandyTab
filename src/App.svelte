aaa06da7d0e57171f45c8bd176ed8d5436990698
daniel <daniel.p.henricks@vanderbilt.edu>
2023-04-28 19:36:02 -0500

    Fixed the opacity of the setup page

--- src/App.svelte
+++ src/App.svelte
@@ -21,13 +21,30 @@
 </script>
 
 <html lang="en-us">
-  {#if !showSetup}
-    <Clock />
-    <Greeting />
-    <ButtonGroup />
-    <Tables />
-  {/if}
-  {#if showSetup}
-    <Setup />
-  {/if}
+  <main>
+    {#if !showSetup}
+      <Clock />
+      <Greeting />
+      <ButtonGroup />
+      <Tables />
+    {/if}
+    {#if showSetup}
+      <div class="container">
+        <Setup />
+      </div>
+    {/if}
+  </main>
 </html>
+
+<style>
+  .container::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(255, 255, 255, 0.4);
+    z-index: -1;
+  }
+</style>
